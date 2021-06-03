d6fe1e424b25d43bf1c6c7b2576076e77068a0ff
Pravin Malkar <pmalkar@digite.com>
2021-06-03 19:28:14 +0530

    lsk-ocp-kata- Committing code change and JTest

---
+++ src/test/java/JTestVehicle.java
@@ -0,0 +1,23 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import com.digite.kata.eg2.Gear;
+import com.digite.kata.eg2.Vehicle;
+
+public class JTestVehicle {
+
+		Vehicle vehicle;
+
+		@Before
+		public void setup() {
+			vehicle = new Vehicle();
+		}
+
+		@Test
+		public void testChangeGear() {
+			vehicle.changeGear(Gear.D);
+			assertTrue(Gear.D.equals(vehicle.getGear()));
+		}
+
+}
