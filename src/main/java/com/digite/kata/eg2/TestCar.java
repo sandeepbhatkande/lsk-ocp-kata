0487ba4c7dbef46e6a762dd4b94eeb24b3b68faa
skjadhav282 <skjadhav282@gmail.com>
2021-06-03 13:43:35 +0530

    adding test case for exception

--- src/main/java/com/digite/kata/eg2/TestCar.java
+++ src/main/java/com/digite/kata/eg2/TestCar.java
@@ -12,4 +12,21 @@ public class TestCar {
         w_car.changeGear(Gear.D);
         Assert.assertEquals(w_car.getGear(), Gear.D);
     }
+    
+    @Test
+    public void testChangeGearForException()
+    {
+        try
+        {
+            Car w_car = new Car();
+            w_car.changeGear(Gear.D);
+            w_car.changeGear(Gear.R);
+                   
+        }
+        catch (Exception a_ex)
+        {
+            Assert.assertEquals(a_ex.getMessage(), "Can't change to REVERSE gear when D gear is engaged!");
+        }
+
+    }
 }
