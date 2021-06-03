65366fdaa8591e27676e7071858149d07de309b4
skjadhav282 <skjadhav282@gmail.com>
2021-06-03 13:20:23 +0530

    Test2

--- src/main/java/com/digite/kata/eg2/TestPlane.java
+++ src/main/java/com/digite/kata/eg2/TestPlane.java
@@ -12,4 +12,14 @@ public class TestPlane
         w_plane.changeGear(Gear.D);
         Assert.assertEquals(w_plane.getGear(), Gear.D);
     }
+    
+     @Test
+    public void testChangeGearwithOverride()
+    {
+        // A plane can reverse engine gear while moving forward, no problem here
+        Plane w_plane = new Plane();
+        w_plane.changeGear(Gear.D);
+        w_plane.changeGear(Gear.R);
+        Assert.assertEquals(w_plane.getGear(), Gear.R);
+    }
 }
