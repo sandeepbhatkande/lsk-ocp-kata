c6db275cf63ee387573f1ac29253c85eda16dccc
skjadhav282 <skjadhav282@gmail.com>
2021-06-03 13:39:41 +0530

    Adding Logic to pass test 3

--- src/main/java/com/digite/kata/eg2/Car.java
+++ src/main/java/com/digite/kata/eg2/Car.java
@@ -7,5 +7,7 @@ public class Car extends Vehicle {
         if(Gear.R.equals(gear) && getGear().equals(Gear.D)){
             throw new RuntimeException("Can't change to REVERSE gear when " + getGear().toString() + " gear is engaged!");
         }
+        else
+            super.changeGear(gear);
     }
 }
