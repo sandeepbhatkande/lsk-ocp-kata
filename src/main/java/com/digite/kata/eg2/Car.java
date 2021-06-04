eb1ebcd3324fb06727b5f0981906e662d9e719e4
ninad joshi <njoshi@digite.com>
2021-06-04 09:31:47 +0530

    isk ocp assignment

--- src/main/java/com/digite/kata/eg2/Car.java
+++ src/main/java/com/digite/kata/eg2/Car.java
@@ -7,5 +7,6 @@ public class Car extends Vehicle {
         if(Gear.R.equals(gear) && getGear().equals(Gear.D)){
             throw new RuntimeException("Can't change to REVERSE gear when " + getGear().toString() + " gear is engaged!");
         }
+		super.changeGear(gear);
     }
 }
